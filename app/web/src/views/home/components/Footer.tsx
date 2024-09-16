742fd50a9dc5de91b2699fb788512612d4bbd8c6
Oscar A. Rodriguez <52334508+AndresKenji@users.noreply.github.com>
2024-09-15 20:26:41 -0500

    footer fixed bottom

--- app/web/src/views/home/components/Footer.tsx
+++ app/web/src/views/home/components/Footer.tsx
@@ -1,7 +1,7 @@
 
 export const Footer = () => {
   return (
-    <div className="container">
+    <div className="container fixed-bottom">
         <footer className="py-3 my-4">
             <ul className="nav justify-content-center border-bottom pb-3 mb-3">
                 <li className="nav-item">
